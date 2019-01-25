bc18b1d015234a3ac033742561a32c9f68e1034e
Dhanashree1991 <dhnpatil01@gmail.com>
2019-01-25 14:58:47 +0530

    Second Commit

---
+++ src/com/jbk/MyDomParser.java
@@ -0,0 +1,60 @@
+package com.jbk;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class MyDomParser {
+
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		
+		try {
+	
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("abc.xml");	
+			NodeList personList = doc.getElementsByTagName("person");
+			for(int i=0;i<personList.getLength();i++) {
+				Node p = personList.item(i);
+				
+				if (p.getNodeType()==Node.ELEMENT_NODE) {
+					Element person = (Element)p;
+					String id = person.getAttribute("id");
+					NodeList nameList = person.getChildNodes();
+					for(int j=0; j<nameList.getLength();j++) {
+						
+						Node n = nameList.item(j);
+						
+						if(n.getNodeType()==Node.ELEMENT_NODE) {
+							Element name = (Element) n;
+							
+					   System.out.println("Person" + id + ":" +name.getTagName() + "=" +name.getTextContent());		
+							
+						}
+					}
+					
+					
+				}
+			} 
+		}catch(ParserConfigurationException e)
+			{
+				e.printStackTrace();
+			}
+		catch(SAXException e) {
+			
+				
+			e.printStackTrace();
+			
+		}
+	}
+
+}
